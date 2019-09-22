b69a9563ccb695b64229ddaeded1904c189422a4
Spider <blackspider9678@gmail.com>
2019-09-21 23:58:37 -0600

    Updated to 1.14.4

---
+++ src/main/java/com/blackspider/egyptian/blocks/BlockList.java
@@ -0,0 +1,10 @@
+package com.blackspider.egyptian.blocks;
+
+import net.minecraft.block.Block;
+
+public class BlockList 
+{
+	public static Block small_pedestal_block;
+	public static Block sun_bathed_gold_block;
+
+}
